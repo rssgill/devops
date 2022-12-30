79f2293b079f40e1f03ac2b8ef5e8ac9bd5bb518
GRFC <GRFC@10.0.0.88>
2022-12-29 21:34:40 -0800

    uploading first.java

---
+++ bmsDeployment/src/main/java/bmsDeployment/First.java
@@ -0,0 +1,13 @@
+package bmsDeployment;
+
+public class First {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi, This my first Java code in the Devops sec class");
+
+	}
+
+}
